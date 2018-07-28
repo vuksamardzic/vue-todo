c895ea6fbdddc48a0a002ec63b3ac6af12a1d02d
Vuk Samardžić <samardzic.vuk@gmail.com>
2018-07-28 20:36:30 +0200

    feat: root component includes, style and functionality

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,63 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <h3 class="mt-2">vue todo</h3>
+          <small class="text-info">simple project made in vue.js (<a href="https://github.com/vuksamardzic/vue-todo">source code</a>)</small>
+          <todo-form @refresh-todo="execGetTodos"/>
+          <p v-if="!data.length"
+             class="small">Your todo list looks empty, let's add some :)</p>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item"
+                v-for="(i, index) in data"
+                :class="{'bt-0': index === 0}"
+                :key="i.id">
+              <todo @refresh-todo="execGetTodos"
+                    :todo="i"/>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import 'bootstrap/dist/css/bootstrap.min.css';
+  import TodoForm from './components/todo-form';
+  import Todo from './components/todo';
+  import axios from 'axios';
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    name: 'app',
+    data() {
+      return {
+        data: []
+      };
+    },
+    methods: {
+      execGetTodos() {
+        axios.get('http://localhost:3000/api/v1/todo')
+          .then(res => {
+            this.data = res.data;
+          });
+      }
+    },
+    mounted() {
+      this.execGetTodos();
+    },
+    components: { TodoForm, Todo }
+  };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .bt-0 {
+    border-top: 0 !important;
+  }
+
+  .c-pointer {
+    cursor: pointer;
+  }
 </style>
